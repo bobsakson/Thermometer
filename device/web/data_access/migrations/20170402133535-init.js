0d3aec74356b788e19bb19b64e51d3b2eb1c10d7
Bob <mckybob@gmail.com>
2017-04-02 22:04:04 -0500

    Switching to mySql, added initial migrations with db-migrate

---
+++ device/web/data_access/migrations/20170402133535-init.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('sessions', {
+    id: { type: 'int', primaryKey: true, unsigned: true },
+    sessionDate: 'timestamp'
+  })
+  .then(
+    function(result) {
+      db.createTable('probes', {
+        id: { type: 'int', primaryKey: true, unsigned: true },
+        name: 'string'
+      });
+    },
+    function(err) {
+      return;
+    }
+  )
+  .then(
+    function(result) {
+      db.createTable('probereadings', {
+        id: { type: 'int', primaryKey: true, unsigned: true },
+        temperature: 'decimal',
+        probeId: {
+                    type: 'int',
+                    unsigned: true,
+                    foreignKey: {
+                        name: 'probeIdForeignKey',
+                        table: 'probes',
+                        rules: {
+                            onDelete: 'RESTRICT'
+                        },
+                        mapping: 'id'
+                    }
+                  },
+        sessionId: {
+                    type: 'int',
+                    unsigned: true,
+                    foreignKey: {
+                        name: 'sessionIdForeignKey',
+                        table: 'sessions',
+                        rules: {
+                            onDelete: 'RESTRICT'
+                        },
+                        mapping: 'id'
+                    }
+                  }
+      });
+    },
+    function(err) {
+      return;
+    }
+  );
+};
+
+exports.down = function(db) {
+  return db.dropTable('probereadings')
+    .then(
+      function(result) {
+        db.dropTable('probes');
+      },
+      function(err) {
+        return;
+      }
+    ).then(
+      function(result) {
+        db.dropTable('sessions');
+      },
+      function(err) {
+        return;
+      }
+    );
+};
+
+exports._meta = {
+  "version": 1
+};
